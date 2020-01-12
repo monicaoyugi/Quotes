0eb077b8a6f129dc432a2eec9624f780ae1881ed
monicaoyugi <monicaoyugi@gmail.com>
2020-01-12 15:48:48 +0300

    added id: number;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuotesComponent } from './quotes/quotes.component';
+import { QuotesDetailComponent } from './quotes-detail/quotes-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuotesComponent
+    QuotesComponent,
+    QuotesDetailComponent
   ],
   imports: [
     BrowserModule,
