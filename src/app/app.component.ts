e8d24b483fbe468591d450d2aeada1a212c4c17c
monicaoyugi <monicaoyugi@gmail.com>
2020-01-14 07:52:34 +0300

    exported app module component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,10 @@
 import { Component } from '@angular/core';
-import { Quotes} from './quotes';
-import { Quote } from '@angular/compiler';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-
 export class AppComponent {
- 
-  constructor() {}
-
-
+  title = 'Quotes';
 }
