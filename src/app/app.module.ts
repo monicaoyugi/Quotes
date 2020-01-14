7baf6b4578464c27c34a6ff161659447443b9850
monicaoyugi <monicaoyugi@gmail.com>
2020-01-14 07:57:35 +0300

    modified e2e/src/app.e2e-spec.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import{FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { QuotesComponent } from './quotes/quotes.component';
-import { QuotesDetailComponent } from './quotes-detail/quotes-detail.component';
+import { QuoteMainComponent } from './quote-main/quote-main.component';
+import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { HighestvoteDirective } from './highestvote.directive';
+import { DateCountPipe } from './date-count.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuotesComponent,
-    QuotesDetailComponent
+    QuoteMainComponent,
+    QuoteDetailComponent,
+    QuoteFormComponent,
+    HighestvoteDirective,
+    DateCountPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
